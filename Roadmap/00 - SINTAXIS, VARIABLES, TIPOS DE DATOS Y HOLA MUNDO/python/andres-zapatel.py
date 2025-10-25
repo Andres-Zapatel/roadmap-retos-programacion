3072214b2fac3e6ac118d7f0903a6eb4a55805dc
Andres-Zapatel <azov760@gmail.com>
2025-10-25 13:48:05 -0500

    Create andres-zapatel.py
    practica de lenguaje python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/andres-zapatel.py
@@ -0,0 +1,16 @@
+https://www.python.org/
+
+Comentario en varias líneas
+
+"""
+Esto es un
+un comentario
+en varias líneas
+"""
+'''
+Esto es un comentario
+'''
+
+my_variable = "Mi varaible"
+my_variable = "Nuevo valor de mi variable"
+
